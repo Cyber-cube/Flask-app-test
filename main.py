d7bc0537a9e342c290b5ec83e103ba8c4422b9f7
Cyber-cube <112477661+Cyber-cube@users.noreply.github.com>
2024-01-18 16:20:23 +0000

    Change

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "Hello, world!"
+  return f"Hello, {os.getenv("PORT")}"
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080)
+  app.run(host="0.0.0.0", port=os.getenv("PORT"))
